0a5707e84d6c5c128770b506b297de1925c8e0e9
Pavan kumar <kondurupavanraju@gmail.com>
2022-05-16 08:23:58 +0530

    added csp header

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+  frame-ancestors 'https:google.com';
+`;
+
 const securityHeaders = [{
     key: 'X-Frame-Options',
     value: 'SAMEORIGIN'
-  }];
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+];
 
 module.exports = {
   async headers() {
